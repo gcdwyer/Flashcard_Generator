f8de9e4c831d6b4067ca17009a7abcfaea62b0a0
gcdwyer <gcdwyer@gmail.com>
2017-10-08 18:28:42 -0400

    add main.js and will write to pull questions from json file

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,56 +1,8 @@
-var inquirer = require('inquirer');
+var fs = require("fs");
 
-function ClozeCard(text, cloze) {
+var ClozeCard = function(text, cloze) {
 	this.text = text;
 	this.cloze = cloze;
 }
-
-var question1 = new ClozeCard("... was the 7th president of the United States.", "Andrew Jackson");
-var question2 = new ClozeCard("... was the 30th president of the United States.", "Calvin Coolidge");
-var question3 = new ClozeCard("... was the 40th president of the United States.", "Ronald Reagan");
-
-inquirer.prompt([
-
-		{
-			type: "input",
-			message: question1.text,
-			name: "q1"
-		}, 
-		{
-			type: "input",
-			message: question2.text,
-			name: "q2"
-		}, 
-		{
-			type: "input",
-			message: question3.text,
-			name: "q3"
-		}, 
-
-]).then(function(answers) {
-
-    if (answers.q1 === question1.cloze) {
-    	console.log("Yes, Andrew Jackson was the 7th president");
-    } else {
-    	console.log("No, Andrew Jackson was the 7th president");
-    }
-
-    if (answers.q2 === question2.cloze) {
-    	console.log("Yes, Calvin Coolidge was the 30th president");
-    } else {
-    	console.log("No, Calvin Coolidge was the 30th president");
-    } 
-
-    if (answers.q3 === question3.cloze) {
-    	console.log("Yes, Ronald Reagan was the 40th president");
-    } else {
-    	console.log("No, Ronald Reagan was the 40th president");
-    }
-
-});
-
-
-
-
 
 module.exports = ClozeCard;
---
+++ main.js
@@ -0,0 +1,33 @@
+// NPM packages
+var fs = require("fs");
+var inquire = require("inquire");
+// Constructor links
+var BasicCard = require("./BasicCard.js");
+var ClozeCard = require("./ClozeCard.js");
+
+var startFlash = function() {
+
+	//TBD
+	//Handles the game starting to select basic or cloze
+	//Inquire to select which flash
+	//switch to handle what is selected
+
+};
+
+var basicFlash = function () {
+
+	//TBD
+	//Read JSON file
+	//Inquire to answer questions
+
+};
+
+var clozeFlash = function() {
+
+	//TBD
+	//Read JSON file
+	//Inquire to answer questions
+
+};
+
+startFlash();
