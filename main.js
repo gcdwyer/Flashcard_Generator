8d8e0c197118422a37ca9531b9451a8176ac17ce
gcdwyer <gcdwyer@gmail.com>
2017-10-08 23:30:21 -0400

    final commit

--- main.js
+++ main.js
@@ -4,8 +4,14 @@ var inquirer = require('inquirer');
 // Constructor links
 var BasicCard = require("./BasicCard.js");
 var ClozeCard = require("./ClozeCard.js");
+// tracks correct answers
+var score = 0;
+
 //Handles the game starting to select basic or cloze
 var startFlash = function() {
+
+	// Used to clear terminal window
+	console.log('\033[2J');
 
 	//Inquire to select which flash
 	inquirer.prompt([
@@ -19,20 +25,16 @@ var startFlash = function() {
 
 	]).then(function (answers) {
 
-		// console.log("you got to startflash promise");
-		// console.log(answers.start);
-
 		//switch to handle what is selected
 		switch (answers.start) {
-
+			// runs basic flash cards
 			case "Take a Basic Quiz":
 				basicFlash(0);
 				break;
-
+			// runs cloze flash cards
 			case "Take a Cloze Quiz":
 				clozeFlash(0);
 				break;
-
 			default:
 				console.log("Opps, you broke it");	
 		}
@@ -41,29 +43,29 @@ var startFlash = function() {
 		console.log('\033[2J');
 
 	});
-
 };
-
+// Handles when basic flash is selected
 var basicFlash = function (count) {
-
+	// reads the JSON file
 	fs.readFile("./basic.json", "utf8", function(error, data) {
 
 		if (error) throw error;
-		var score = 0;
-		var test = JSON.parse(data);
-		if (count < test.length) {
-
+		
+		var questionInfo = JSON.parse(data);
+		// if current quesiton is less than total
+		if (count < questionInfo.length) {
+			// prompt user to answer questions
 			inquirer.prompt([
 
 			{
 				name: "question",
-				message: test[count].front + "\nAnswer:",
+				message: questionInfo[count].front + "\nAnswer:",
 				type: "input",
 			}
 
 			]).then(function(answers) {
-
-				if (answers.question.toLowerCase() === test[count].back.toLowerCase()) {
+				// if user input = answer score +1
+				if (answers.question.toLowerCase() === questionInfo[count].back.toLowerCase()) {
 					console.log("--------------------------------------------------------------");
 					console.log("You are correct!");
 					console.log("--------------------------------------------------------------");
@@ -72,80 +74,111 @@ var basicFlash = function (count) {
 					basicFlash(count);
 				} else {
 					console.log("--------------------------------------------------------------");
-					console.log("Incorrect! The answer is '" + test[count].back + "'");
+					console.log("Incorrect! The answer is '" + questionInfo[count].back + "'");
 					console.log("--------------------------------------------------------------");
 					count++;
 					basicFlash(count);
 				}
 			});
-
-		} else if (count == test.length) {
+		// if last question, ask to play again and show score	
+		} else if (count == questionInfo.length) {
 
 			console.log("Game Over!");
-			console.log("You got " + score + " out of " + test.length + " correct.");
+			console.log("You got " + score + " out of " + questionInfo.length + " correct.");
 
 			inquirer.prompt([
 
 			{
-				name: "playAgain",
+				name: "again",
 				message: "Play Again?",
-				type: "input",
+				type: "confirm" // confirm returns a boolean
 			}
 
 			]).then(function(answers) {
-
-				// if yes then run startflash
-				// else exit
+				
+				if (answers.again === true) {
+					score = 0;
+					startFlash();
+				} else {
+					process.exit()
+				}
 
 			});
 
-
-
 		} else {
-
-			console.log("TBD");
-
+			// Failover incase game gets in weird state
+			startFlash();
 		}
 	});
 };
 
-
+// Handles when cloze flash is selected
 var clozeFlash = function (count) {
-
+	// reads the JSON file
 	fs.readFile("./cloze.json", "utf8", function(error, data) {
 
 		if (error) throw error;
-		var test = JSON.parse(data);
-		if (count < test.length) {
-
-			var newClozeCard = new ClozeCard(test[count].text, test[count].cloze);
-
+		var questionInfo = JSON.parse(data);
+		// if current quesiton is less than total
+		if (count < questionInfo.length) {
+			// new object created from the clozecard constructor
+			var newClozeCard = new ClozeCard(questionInfo[count].text, questionInfo[count].cloze);
+			// prompt user to answer questions
 			inquirer.prompt([
 
 			{
 				name: "question",
-				message: newClozeCard.dotText(),
+				message: newClozeCard.dotText() + "\nAnswer:",
 				type: "input"
 			}
 
 			]).then(function(answers) {
-
+				// if user input = answer score +1
 				if (answers.question.toLowerCase() === newClozeCard.cloze.toLowerCase()) {
-					console.log("-------------------------------");
-					console.log("Correct");
-					console.log("-------------------------------");
+					console.log("--------------------------------------------------------------");
+					console.log("You are correct!");
+					console.log("--------------------------------------------------------------");
 					count++;
+					score++
 					clozeFlash(count);
 				} else {
-					console.log("-------------------------------");
-					console.log("Incorrect");
-					console.log("-------------------------------");
+					console.log("==============================================================");
+					console.log("Incorrect! The answer is '" + newClozeCard.cloze + "'");
+					console.log("==============================================================");
 					count++;
 					clozeFlash(count);
 				}
 			});
+		// if last question, ask to play again and show score
+		} else if (count == questionInfo.length) {
+
+			console.log("Game Over!");
+			console.log("You got " + score + " out of " + questionInfo.length + " correct.");
+
+			inquirer.prompt([
+
+			{
+				name: "again",
+				message: "Play Again?",
+				type: "confirm" // confirm returns a boolean
+			}
+
+			]).then(function(answers) {
+				
+				if (answers.again === true) {
+					score = 0;
+					startFlash();
+				} else {
+					process.exit()
+				}
+
+			});
+
+		} else {
+			// Failover incase game gets in weird state
+			startFlash();
 		}
 	});
 };
-
+// Runs the game
 startFlash();
